fe5aeac06eb341992895a7220ace5002b7264185
David Cooper <david.cooper@curtin.edu.au>
2021-09-24 17:41:31 +0800

    Fix: the pruner extension wasn't recursing properly; now it is.

--- lib/pruner.py
+++ lib/pruner.py
@@ -6,19 +6,23 @@ class PrunerTreeProcessor(markdown.treeprocessors.Treeprocessor):
     def __init__(self, md, pruneClasses: set[str]):
         super().__init__(md)
         self.pruneClasses = pruneClasses
-
+        
     def run(self, root):
         pruneElements = []
         for element in root:            
             elementClasses = (element.attrib.get('class') or '').split()
             if not self.pruneClasses.isdisjoint(elementClasses):
-                #element.clear()
                 pruneElements.append(element)
                 
         for element in pruneElements:
             root.remove(element)
+            
+        # Recurse
+        for element in root:
+            self.run(element)
                 
         return None
+    
 
 
 class PrunerExtension(markdown.Extension):
