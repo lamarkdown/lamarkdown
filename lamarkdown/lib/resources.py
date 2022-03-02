7dafec74c8467f871efd97721ff089c13b611c97
David Cooper <david.cooper@curtin.edu.au>
2022-03-02 17:59:36 +0800

    Fixed resource-embedding so that (a) it doesn't happen by default, (b) it doesn't happen at all for remote resources (for now), and (c) embedded resources are kept in a consistent order with already-inlined resources.

--- lamarkdown/lib/resources.py
+++ lamarkdown/lib/resources.py
@@ -23,31 +23,49 @@ def embed_res_list(embedded_resources: List[Resource],
                    base_path: str):
 
     i = 0
+    j = 0
     while i < len(linked_resources):
         res = linked_resources[i]
 
+        # Note: 'policy' and 'res.embed' can each be True, False or None. Hence the more explicit 
+        # checking below.
+        
         link = res.value
-        if link is not None and (policy is True or (policy is not False and res.embed is not False)):
+        if (link is not None and 
+            (policy is True or (policy is not False and res.embed is True)) and
+            not _URL_SCHEME_REGEX.match(link)):
 
-            print(f'link={link}, policy={policy}, res.embed={res.embed}')
+            #if _URL_SCHEME_REGEX.match(link):
+                #with urllib.request.urlopen(link) as conn:
+                    ## TODO: handle exceptions, timeouts, caching, progress reporting, encoding
+                    #text = conn.read().decode()
+            #else:
+                #with open(os.path.join(base_path, link)) as reader:
+                    #text = reader.read()
 
-            if _URL_SCHEME_REGEX.match(link):
-                with urllib.request.urlopen(link) as conn:
-                    # TODO: handle exceptions, timeouts, caching, progress reporting, encoding
-                    text = conn.read().decode()
-            else:
-                with open(os.path.join(base_path, link)) as reader:
-                    text = reader.read()
-
-            embedded_resources.append(Resource(
-                value_factory = lambda _: text,
-                xpaths        = res.xpaths,
-                reified       = True,
-                value         = text))
+            with open(os.path.join(base_path, link)) as reader:
+                text = reader.read()
+                    
+            # Insert rather than append, to try to maintain the ordering, which may be important.
+            # That is, linked resources would occur before inline resources, and so newly-embedded
+            # resources should also occur before existing inline resources.
+            embedded_resources.insert(
+                j,
+                Resource(
+                    value_factory = lambda _: text,
+                    xpaths        = res.xpaths,
+                    reified       = True,
+                    value         = text
+                )
+            )
+            j += 1
             linked_resources.pop(i)
 
         else:
-            # Either the build file says not to embed, or the resource has been filtered out.
+            # Either:
+            # (a) the resource has been filtered out, or
+            # (b) the build file says not to embed, or 
+            # (c) the resource is remote.
             i += 1
 
 
