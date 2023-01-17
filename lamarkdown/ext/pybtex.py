10c5dca9c87af20294cd66cc7516f97c5ed0aa22
David Cooper <david.cooper@curtin.edu.au>
2023-01-17 15:24:00 +0800

    Pybtex extension now has (optional) forward and back-linking, a 'place marker' you can use to determine where the reference list should go, optional extra text inside each citation, and non-matching citations -- those that cannot be found in the database -- are left-alone. Also, some refactoring happened.

--- lamarkdown/ext/pybtex.py
+++ lamarkdown/ext/pybtex.py
@@ -2,35 +2,59 @@
 # Pybtex Extension
 
 '''
+from lamarkdown.lib.progress import Progress
 
 import markdown
 from markdown.inlinepatterns import InlineProcessor
 from markdown.treeprocessors import Treeprocessor
 
 import pybtex.backends.html
+import pybtex.database
 import pybtex.plugin
+import pybtex.style.formatting
 
 import lxml.html
 
 import io
+import os.path
 import re
 from typing import List
 from xml.etree import ElementTree
 
 
 class CitationInlineProcessor(InlineProcessor):
-    REGEX = r'\[@([^]]+)\]'
+   
+    # A citation syntactically consists of '[@' <citekey> [<extra>] ']'.
+    # Where <citekey> is defined as per https://metacpan.org/dist/Text-BibTeX/view/btparse/doc/bt_language.pod
+    # _except_ that '[' and ']' are invalid, since we use them to enclose a citation.
+    #
+    # <extra> is additional free-form text, starting with an invalid citekey character 
+    # (such as ' ', ',' or '\'). This text will be added to the citation.
     
-    def __init__(self, cited_keys: List[str]):
+    #REGEX = '\\[@([^\x00-\x20 "#%\'(),=[\\]\\\\{}~]+)([^]]*)\\]'
+    
+    REGEX = '\\[@([a-zA-Z0-9!$&*+./:;<>?^_`|-]+)\\\\?([^]]*)\\]'
+    
+    def __init__(self, bib_data: pybtex.database.BibliographyData, 
+                       cited_keys: List[str]):
         super().__init__(self.REGEX)
+        self.bib_data = bib_data
         self.cited_keys = cited_keys
         
     def handleMatch(self, match, data):
         key = match.group(1)
-        self.cited_keys.append(key)
-        elem = ElementTree.Element('cite')
-        elem.attrib['key'] = key
-        return elem, match.start(0), match.end(0)
+        if key in self.bib_data.entries:
+            extra = match.group(2)
+            self.cited_keys.append(key)
+            elem = ElementTree.Element('cite')
+            elem.attrib['key'] = key
+            elem.attrib['extra'] = extra
+            return elem, match.start(0), match.end(0)
+    
+        else:
+            # If the apparent citekey isn't in the reference database, then just leave everything
+            # as it is. 
+            return None, None, None
         
         
 class ModifiedPybtexHtmlBackend(pybtex.backends.html.Backend):
@@ -45,46 +69,27 @@ class ModifiedPybtexHtmlBackend(pybtex.backends.html.Backend):
         
 
 class PybtexTreeProcessor(Treeprocessor):
-    def __init__(self, md, ext, cited_keys: List[str]):
+    def __init__(self, md, 
+                       ext: 'PybtexExtension', 
+                       bib_data: pybtex.database.BibliographyData,
+                       bib_style: pybtex.style.formatting.BaseStyle,
+                       cited_keys: List[str]):
         super().__init__(md)
         self.ext = ext
+        self.bib_data = bib_data
+        self.bib_style = bib_style
         
         # 'cited_keys' will be empty at first, but in between here and run() below, 
         # CitationInlineProcessor will add all citations found in the document.
         self.cited_keys = cited_keys
         
     def run(self, root):
-        # 'cited_keys' will have been populated by now.
+        # 'cited_keys' should have been populated by now. If it's still empty, it means there are
+        # no citations, and we can cut short the Treeprocessor:
+        if len(self.cited_keys) == 0:
+            return
         
-        file_spec = self.ext.getConfig('file')
-        if file_spec is None:
-            files = []
-        elif isinstance(file_spec, str):
-            files = [file_spec]
-        else:
-            files = list(file_spec)
-        
-        ref_str = self.ext.getConfig('references')
-        if ref_str:
-            files.append(io.StringIO(ref_str))
-        
-        bib_parser = pybtex.plugin.find_plugin('pybtex.database.input', self.ext.getConfig('format'))
-        bib_data = bib_parser(
-            encoding = self.ext.getConfig('encoding'),
-            wanted_entries = self.cited_keys,
-            min_crossrefs = self.ext.getConfig('min_crossrefs')).parse_files(files)
-        
-        # TODO: handle errors from parse_files(): pybtex.database.BibliographyDataError and OSError
-        
-        style_cls = pybtex.plugin.find_plugin('pybtex.style.formatting', self.ext.getConfig('style'))
-        style = style_cls(
-            label_style      = self.ext.getConfig('label_style'),
-            name_style       = self.ext.getConfig('name_style'),
-            sorting_style    = self.ext.getConfig('sorting_style'),
-            abbreviate_names = self.ext.getConfig('abbreviate_names'),
-            min_crossrefs    = self.ext.getConfig('min_crossrefs')
-        )
-        formatted_biblio = style.format_bibliography(bib_data, self.cited_keys)
+        formatted_biblio = self.bib_style.format_bibliography(self.bib_data, self.cited_keys)
         
         # Populate the <cite> elements (created by CitationInlineProcessor) with the 'labels' 
         # created by Pybtex.
@@ -94,26 +99,24 @@ class PybtexTreeProcessor(Treeprocessor):
         
         for elem in root.iter(tag = 'cite'):
             key = elem.attrib['key']
-            if key:
-                del elem.attrib['key']
-                label = entries[key].label
-                n_citations[label] = n_citations.get(label, 0) + 1                    
-                elem.attrib['id'] = f'pybtexcite:{label}-{n_citations[label]}'
-                
-                if create_forward_links:                    
-                    elem.text = '['
-                    link = ElementTree.SubElement(elem, 'a', attrib = {'href': f'#pybtexref:{label}'})
-                    link.text = label
-                    link.tail = ']'
-                else:
-                    elem.text = f'[{label}]'
-                
+            extra = elem.attrib['extra']
+            
+            del elem.attrib['key']
+            del elem.attrib['extra']
+            
+            label = entries[key].label
+            n_citations[label] = n_citations.get(label, 0) + 1                    
+            elem.attrib['id'] = f'pybtexcite:{label}-{n_citations[label]}'
+            
+            if create_forward_links:                    
+                elem.text = '['
+                link = ElementTree.SubElement(elem, 'a', attrib = {'href': f'#pybtexref:{label}'})
+                link.text = label + extra
+                link.tail = ']'
             else:
-                pass
-                # TODO: insert error text explaining that 'key' was not a valid citekey.
-                
+                elem.text = f'[{label + extra}]'
+                                
         # TODO: we also want:
-        # * hyperlinks (in both directions) between the citations and the reference entries
         # * hover popups (if possible; maybe just store the entry in the <cite> element's 'title' attribute?)
         # * configurable citation formats; e.g., as per the Latex natbib package.
               
@@ -126,8 +129,8 @@ class PybtexTreeProcessor(Treeprocessor):
         # designed to parse HTML, and gets confused on '&nbsp;').
         biblio_tree = lxml.html.fromstring(biblio_html.getvalue())
         
+        # Create back-links from references to their related citations.
         create_back_links = self.ext.getConfig('hyperlinks') in ['both', 'back']
-        
         for dd in biblio_tree.iterfind('.//dd'):
             label = dd.attrib['label']
             del dd.attrib['label']
@@ -150,56 +153,169 @@ class PybtexTreeProcessor(Treeprocessor):
                         back_link = lxml.etree.SubElement(span, 'a', attrib = {'href': f'#pybtexcite:{label}-{i}'})
                         back_link.text = str(i)
                         back_link.tail = ' '
+                    back_link.tail = ''
+                    
+        # Determine where to put the bibliography -- the element containing the 'place_marker' 
+        # text -- or (if not found) at the end of the document.
+        placeholder = self.ext.getConfig('place_marker')
+        def find_biblio_root(elem):
+            if len(elem) == 0:
+                if elem.text == placeholder:
+                    return elem
+            else:
+                for child in elem:
+                    found_elem = find_biblio_root(child)
+                    if found_elem is not None:
+                        return found_elem
+            return None
+        
+        biblio_root = find_biblio_root(root)
+        if biblio_root is None:
+            biblio_root = ElementTree.SubElement(root, 'dl')            
+        else:
+            biblio_root.tag = 'dl'
+            biblio_root.text = ''
                         
         # Copy the LXML-parsed tree to the (standard) xml.etree structure used by Python-Markdown. 
         # AFAIK, there's no shortcut for this.
         def copy_tree(xml_tree_dest, lxml_tree_src):
-            dest_element = ElementTree.SubElement(xml_tree_dest, 
-                                                  lxml_tree_src.tag, 
-                                                  dict(lxml_tree_src.attrib))
-            dest_element.text = lxml_tree_src.text
-            dest_element.tail = lxml_tree_src.tail
             for src_child in lxml_tree_src:
+                dest_element = ElementTree.SubElement(xml_tree_dest, 
+                                                      src_child.tag, 
+                                                      dict(src_child.attrib))
+                dest_element.text = src_child.text
+                dest_element.tail = src_child.tail
                 copy_tree(dest_element, src_child)
 
-        # Append <dl> element (the bibliography) to the root
-        # TODO: we actually want to look for a placeholder to replace (and only append as a fallback).
-        copy_tree(root, biblio_tree.find('.//dl'))
+        copy_tree(biblio_root, biblio_tree.find('.//dl'))
         
-    
+        # Add <dl> element (the bibliography) to the root
+        # TODO: we actually want to look for a placeholder to replace (and only append as a fallback).
+
     
 class PybtexExtension(markdown.Extension):
     def __init__(self, **kwargs):
+        p = None
+        try:
+            from lamarkdown.lib.build_params import BuildParams
+            p = BuildParams.current
+        except ModuleNotFoundError:
+            pass # Use default defaults
+
+        progress = p.progress if p else Progress()
+        
         self.config = {
             # Todo: also allow embedded reference information, as a string in the build file.
-            'file':       ['references.bib', 'A string, or list of strings, containing the filename(s) of Bibtex-formatted reference lists.'],
-            'references': ['', 'A string directly containing a Bibtex-formatted reference list (or None).'],
-            'encoding':   ['utf-8-sig', 'Encoding of the reference file.'],
-            'format':     ['bibtex', '...'],
-            'style':      ['unsrt', 'Reference style ("alpha", "plain", "unsrt", "unsrtalpha").'],
-            'label_style': [None, '...'],
-            'name_style':  [None, '...'],
-            'sorting_style': [None, '...'],
-            'abbreviate_names': [False, 'True/False...'],
-            'min_crossrefs': [2, '...'],
-            'hyperlinks': ['both', 'Must be "both" (the default), "forward", "back" or "none", indicating whether to create hyperlinks from citation to reference (forward/both) and from reference back to citation(s) (back/both).']
+            'progress': [p.progress if p else Progress(), 'An object accepting progress messages.'],
+            'file': [
+                'references.bib', 
+                'A string, or list of strings, containing the filename(s) of Bibtex-formatted reference lists.'
+            ],
+            'references': [
+                '', 
+                'A string directly containing a Bibtex-formatted reference list (or None).'
+            ],
+            'ignore_missing_file': [
+                True, 
+                'If True, missing reference files are ignored, rather than reported as errors.'
+            ],
+            'encoding': [
+                'utf-8-sig', 
+                'Encoding of the reference file.'
+            ],
+            'format': [
+                'bibtex', 
+                '...'
+            ],
+            'style': [
+                'unsrt', 
+                'Reference style ("alpha", "plain", "unsrt", "unsrtalpha").'
+            ],
+            'label_style': [
+                None, 
+                '...'
+            ],
+            'name_style': [
+                None, 
+                '...'
+            ],
+            'sorting_style': [
+                None, 
+                '...'
+            ],
+            'abbreviate_names': [
+                False, 
+                'True/False...'
+            ],
+            'min_crossrefs': [
+                2, 
+                '...'
+            ],
+            'place_marker': [
+                '///References Go Here///', 
+                'The text string marking where bibliography entries will be placed.'
+            ],
+            'hyperlinks': [
+                'both', 
+                'Must be "both" (the default), "forward", "back" or "none", indicating whether to create hyperlinks from citation to reference (forward/both) and from reference back to citation(s) (back/both).'
+            ]
         }
         super().__init__(**kwargs)
 
 
     def extendMarkdown(self, md):
         
+        file_spec = self.getConfig('file')
+        if file_spec is None:
+            files = []
+        elif isinstance(file_spec, str):
+            files = [file_spec]
+        else:
+            files = list(file_spec)
+        
+        ref_str = self.getConfig('references')
+        if ref_str:
+            files.append(io.StringIO(ref_str))
+        
+        # Pybtex reference database parser.
+        bib_parser_cls = pybtex.plugin.find_plugin('pybtex.database.input', self.getConfig('format'))
+        bib_parser = bib_parser_cls(
+            encoding = self.getConfig('encoding'),
+            min_crossrefs = self.getConfig('min_crossrefs'))
+        
+        # Parse files one by one.
+        for file in files:
+            if (self.getConfig('ignore_missing_file') 
+                and isinstance(file, str)
+                and not os.path.exists(file)):
+                continue
+            
+            try:
+                bib_parser.parse_file(file)                
+            except Exception as e:
+                self.getConfig('progress').error_from_exception('Pybtex', e)
+                    
+        # Pybtex formatter -- creates the document reference list.
+        bib_style_cls = pybtex.plugin.find_plugin('pybtex.style.formatting', self.getConfig('style'))
+        bib_style = bib_style_cls(
+            label_style      = self.getConfig('label_style'),
+            name_style       = self.getConfig('name_style'),
+            sorting_style    = self.getConfig('sorting_style'),
+            abbreviate_names = self.getConfig('abbreviate_names'),
+            min_crossrefs    = self.getConfig('min_crossrefs')
+        )
+        
         cited_keys = []
 
         # The inline processor just replaces [@...] with <cite key="..." />, and gathers the set
         # of all cited keys.
-        inline_proc = CitationInlineProcessor(cited_keys)
+        inline_proc = CitationInlineProcessor(bib_parser.data, cited_keys)
         md.inlinePatterns.register(inline_proc, 'lamarkdown.pybtex', 130)
 
         # The tree processor must come after the inline processor. Python-Markdown runs all inline
         # processors from within a TreeProcessor named InlineProcessor, with priority 20, so 
         # PybtexTreeProcessor must have lower priority than that.
-        tree_proc = PybtexTreeProcessor(md, self, cited_keys)
+        tree_proc = PybtexTreeProcessor(md, self, bib_parser.data, bib_style, cited_keys)
         md.treeprocessors.register(tree_proc, 'lamarkdown.sections', 10)
 
 
--- tests/ext/test_pybtex.py
+++ tests/ext/test_pybtex.py
@@ -8,6 +8,39 @@ from textwrap import dedent
 
 
 class PybtexTestCase(unittest.TestCase):
+    
+    REFERENCES = r'''
+        @article{refA,
+            author = "The Author A",
+            title = "The Title A",
+            journal = "The Journal A",
+            year = "1990"
+        }
+        @article{refB,
+            author = "The Author B",
+            title = "The Title B",
+            journal = "The Journal B",
+            year = "1991"
+        }
+        @article{refC,
+            author = "The Author C",
+            title = "The Title C",
+            journal = "The Journal C",
+            year = "1992"
+        }
+        @article{refD,
+            author = "The Author D",
+            title = "The Title D",
+            journal = "The Journal D",
+            year = "1993"
+        }
+        @article{refE,
+            author = "The Author E",
+            title = "The Title E",
+            journal = "The Journal E",
+            year = "1994"
+        }
+    '''
 
     def run_markdown(self, markdown_text, **kwargs):
         md = markdown.Markdown(
@@ -18,64 +51,175 @@ class PybtexTestCase(unittest.TestCase):
             }}
         )
         return md.convert(dedent(markdown_text).strip())
-
-
-    def test_single_citation(self):
-        '''Check some sections!'''
-
+    
+    
+    def test_unused(self):
         html = self.run_markdown(
             r'''
             # Heading
-
-            Citation A [@refB], citation B [@refC].
-            
-            Citation C [@refD], citation A [@refB].
-            ''',
-            file = [],
-            references = r'''
-                @article{refA,
-                    author = "The Author A",
-                    title = "The Title A",
-                    journal = "The Journal A",
-                    year = "1990"
-                }
-                @article{refB,
-                    author = "The Author B",
-                    title = "The Title B",
-                    journal = "The Journal B",
-                    year = "1991"
-                }
-                @article{refC,
-                    author = "The Author C",
-                    title = "The Title C",
-                    journal = "The Journal C",
-                    year = "1992"
-                }
-                @article{refD,
-                    author = "The Author D",
-                    title = "The Title D",
-                    journal = "The Journal D",
-                    year = "1993"
-                }
-                @article{refE,
-                    author = "The Author E",
-                    title = "The Title E",
-                    journal = "The Journal E",
-                    year = "1994"
-                }
-            ''')
-            
+            '''
+        )
+        
         self.assertRegex(
             html,
             fr'''(?sx)
             \s* <h1>Heading</h1>
-            \s* <p>Citation[ ]A[ ]<cite[ ]id="pybtexcite:1-1">\[<a[ ]href="\#pybtexref:1">1</a>\]</cite>,[ ]
-                   citation[ ]B[ ]<cite[ ]id="pybtexcite:2-1">\[<a[ ]href="\#pybtexref:2">2</a>\]</cite>.</p>
-            \s* <p>Citation[ ]C[ ]<cite[ ]id="pybtexcite:3-1">\[<a[ ]href="\#pybtexref:3">3</a>\]</cite>,[ ]
-                   citation[ ]A[ ]<cite[ ]id="pybtexcite:1-2">\[<a[ ]href="\#pybtexref:1">1</a>\]</cite>.</p>
-            \s* <dl> \s* <dt[ ]id="pybtexref:1">1</dt> \s* <dd> .* </dl>
+            '''
+        )
+        
+    def test_non_matching_citations(self):
+        
+        # Here, @refX doesn't match anything in the reference database, and we want to retain the 
+        # literal text instead.
+        html = self.run_markdown(
+            r'''
+            # Heading
+
+            Citation B [@refB], citation X [@refX].
+            ''',
+            file = [],
+            references = self.REFERENCES,
+            hyperlinks = 'none')
+        
+        self.assertRegex(
+            html,
+            fr'''(?sx)
+            \s* <h1>Heading</h1>
+            \s* <p>Citation[ ]B[ ]<cite[ ]id="pybtexcite:1-1">\[1]</cite>,[ ]citation[ ]X[ ]\[@refX].</p>
+            \s* <dl> 
+            \s* <dt[ ]id="pybtexref:1">1</dt> \s* <dd> .* \. </dd>
+            \s* </dl>
             \s*
             '''
         )
         
-        # TODO: the actual reference list comes after, so the above will fail at first.
+
+    def test_links(self):
+        linked_citations = r'''
+            \s* <p>Citation[ ]B[ ]<cite[ ]id="pybtexcite:1-1">\[<a[ ]href="\#pybtexref:1">1,[ ]p\.[ ]5</a>\]</cite>,[ ]
+                   citation[ ]C[ ]<cite[ ]id="pybtexcite:2-1">\[<a[ ]href="\#pybtexref:2">2</a>\]</cite>.</p>
+            \s* <p>Citation[ ]D[ ]<cite[ ]id="pybtexcite:3-1">\[<a[ ]href="\#pybtexref:3">3[ ]maybe</a>\]</cite>,[ ]
+                   citation[ ]B[ ]<cite[ ]id="pybtexcite:1-2">\[<a[ ]href="\#pybtexref:1">1</a>\]</cite>.</p>
+        '''
+        
+        unlinked_citations = r'''
+            \s* <p>Citation[ ]B[ ]<cite[ ]id="pybtexcite:1-1">\[1,[ ]p\.[ ]5\]</cite>,[ ]
+                   citation[ ]C[ ]<cite[ ]id="pybtexcite:2-1">\[2\]</cite>.</p>
+            \s* <p>Citation[ ]D[ ]<cite[ ]id="pybtexcite:3-1">\[3[ ]maybe\]</cite>,[ ]
+                   citation[ ]B[ ]<cite[ ]id="pybtexcite:1-2">\[1\]</cite>.</p>
+        '''
+        
+        linked_refs = r'''
+            \s* <dt[ ]id="pybtexref:1">1</dt> \s* <dd> .* [ ]<span>↩[ ]<a[ ]href="\#pybtexcite:1-1">1</a>
+                                                                    [ ]<a[ ]href="\#pybtexcite:1-2">2</a></span></dd>
+            \s* <dt[ ]id="pybtexref:2">2</dt> \s* <dd> .* [ ]<a[ ]href="\#pybtexcite:2-1">↩</a></dd>
+            \s* <dt[ ]id="pybtexref:3">3</dt> \s* <dd> .* [ ]<a[ ]href="\#pybtexcite:3-1">↩</a></dd>
+        '''
+
+        unlinked_refs = r'''
+            \s* <dt[ ]id="pybtexref:1">1</dt> \s* <dd> .* \. </dd>
+            \s* <dt[ ]id="pybtexref:2">2</dt> \s* <dd> .* \. </dd>
+            \s* <dt[ ]id="pybtexref:3">3</dt> \s* <dd> .* \. </dd>
+        '''
+        
+        data = [('both',    linked_citations,   linked_refs),
+                ('forward', linked_citations,   unlinked_refs),
+                ('back',    unlinked_citations, linked_refs),
+                ('none',    unlinked_citations, unlinked_refs)]
+        
+        for hyperlinks, cite_regex, ref_regex in data:
+            html = self.run_markdown(
+                r'''
+                # Heading
+
+                Citation B [@refB, p. 5], citation C [@refC].
+                
+                Citation D [@refD maybe], citation B [@refB].
+                ''',
+                file = [],
+                references = self.REFERENCES,
+                hyperlinks = hyperlinks)
+                
+            self.assertRegex(
+                html,
+                fr'''(?sx)
+                \s* <h1>Heading</h1>
+                {cite_regex}
+                \s* <dl> 
+                {ref_regex}
+                \s* </dl>
+                \s*
+                '''
+            )
+            
+            
+    def test_placeholder(self):
+        src_place_marker = r'///References Go Here///'
+        src_citation_b = r'Citation B [@refB].'
+        src_citation_c = r'Citation C [@refC].'
+        
+        regex_references = r'''
+            \s* <dl>
+            \s* <dt[ ]id="pybtexref:1">1</dt> \s* <dd> .* \. </dd>
+            \s* <dt[ ]id="pybtexref:2">2</dt> \s* <dd> .* \. </dd>
+            \s* </dl>
+        '''
+        
+        regex_citation_b = r'\s* <p>Citation[ ]B[ ]<cite[ ]id="pybtexcite:1-1">\[1]</cite>.</p>'
+        regex_citation_c = r'\s* <p>Citation[ ]C[ ]<cite[ ]id="pybtexcite:2-1">\[2]</cite>.</p>'
+        
+        # We're testing different placements of the 'place marker, which determines 
+        data = [
+            (
+                # Marker at start
+                src_place_marker + '\n\n' + src_citation_b + '\n\n' + src_citation_c,
+                fr'''(?sx)
+                    {regex_references}
+                    {regex_citation_b}
+                    {regex_citation_c}
+                    \s*
+                '''
+            ),
+            (
+                # Marker in the middle
+                src_citation_b + '\n\n' + src_place_marker + '\n\n' + src_citation_c,
+                fr'''(?sx)
+                    {regex_citation_b}
+                    {regex_references}
+                    {regex_citation_c}
+                    \s*
+                '''
+            ),
+            (
+                # Marker at end
+                src_citation_b + '\n\n' + src_citation_c + '\n\n' + src_place_marker,
+                fr'''(?sx)
+                    {regex_citation_b}
+                    {regex_citation_c}
+                    {regex_references}
+                    \s*
+                '''
+            ),
+            (
+                # Marker missing -- should be the same as if it was at the end.
+                src_citation_b + '\n\n' + src_citation_c,
+                fr'''(?sx)
+                    {regex_citation_b}
+                    {regex_citation_c}
+                    {regex_references}
+                    \s*
+                '''
+            )
+        ]
+            
+        for markdown, regex in data:        
+            html = self.run_markdown(
+                markdown,
+                file = [],
+                references = self.REFERENCES,
+                hyperlinks = 'none')
+            
+            self.assertRegex(html, regex)
+            
+            
+        
